72dedfaa3d0bebe39f8dd02c6952922a2564c2b8
Gaven <gavengrantz@gmail.com>
2021-05-13 00:18:53 -0700

    set position to fixed for add task popover
    so it locks with scrolling

--- src/components/SubTasks.js
+++ src/components/SubTasks.js
@@ -1,6 +1,7 @@
 // component for list of subtasks
 import SubTask from './SubTask'
 import AddSubTask from './AddSubTask'
+import Button from '@material-ui/core/Button';
 import AddCircleIcon from '@material-ui/icons/AddCircle';
 import { useState } from 'react';
 
@@ -12,7 +13,7 @@ const SubTasks = ({ subTasks }) => {
             {/* iterates through subtask array and passes each subtask to SubTask component */}
             {subTasks ? subTasks.map((subTask) => (<SubTask subTask={subTask} />)) : ''}
             <div className="addsubtask">
-                {showSubTaskInput ? <AddSubTask /> : <button className="addsubtaskicon" onClick={() => setShowSubTaskInput(true)} ><AddCircleIcon fontSize="large" /></button>}
+                {showSubTaskInput ? <AddSubTask /> : <Button onClick={() => setShowSubTaskInput(true)} ><AddCircleIcon fontSize="large" /></Button>}
             </div>
         </>
     )
