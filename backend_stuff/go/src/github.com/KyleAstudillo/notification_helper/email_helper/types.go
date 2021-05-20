8c21fe71cbbe928dc7d1b447aa1046a3a79f52bc
KyleAstudillo <kyle.astudillo@yahoo.com>
2021-05-20 08:13:12 -0700

    Kyle: Clean Cherry Pick from FE.4.0 onto master

--- backend_stuff/go/src/github.com/KyleAstudillo/notification_helper/email_helper/types.go
+++ backend_stuff/go/src/github.com/KyleAstudillo/notification_helper/email_helper/types.go
@@ -4,12 +4,12 @@ var (
     // Replace sender@example.com with your "From" address.
     // This address must be verified with Amazon SES.
     //Sender = "kyle.astudillo@yahoo.com"
-    Sender String
+    Sender string
 
     // Replace recipient@example.com with a "To" address. If your account
     // is still in the sandbox, this address must be verified.
     //Recipient = "kyle.astudillo@yahoo.com"
-    Recipient String
+    Recipient string
 
     // Specify a configuration set. To use a configuration
     // set, comment the next line and line 92.
@@ -17,17 +17,17 @@ var (
 
     // The subject line for the email.
     //Subject = "Amazon SES Test (AWS SDK for Go)"
-    Subject String
+    Subject string
 
     // The HTML body for the email.
     /*HtmlBody =  "<h1>Amazon SES Test Email (AWS SDK for Go)</h1><p>This email was sent with " +
                 "<a href='https://aws.amazon.com/ses/'>Amazon SES</a> using the " +
                 "<a href='https://aws.amazon.com/sdk-for-go/'>AWS SDK for Go</a>.</p>"*/
-    HtmlBody String
+    HtmlBody string
 
     //The email body for recipients with non-HTML email clients.
     //TextBody = "This email was sent with Amazon SES using the AWS SDK for Go."
-    TextBody String
+    TextBody string
 
     // The character encoding for the email.
     CharSet = "UTF-8"
